d21c81c4ae291068667eacb68dea7b7391190eee
Gyaneshwar5 <83696787+Gyaneshwar5@users.noreply.github.com>
2021-05-05 14:40:04 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,60 @@
+
+name= " Arjun "
+age = 21
+print("My Name is", name, ". I'm of" ,age, "years old.")
+print("My Name is"+ name+ ".")
+print("My Name is {}. I'm of {} years old".format(name,age))
+print(f"My Name is {name}. I'm of {age} years old.")
+
+
+a=154
+b="cbu"
+c=0.254
+print(type(a),type(b),type(c))
+
+a=25
+print(type(a),a)
+b=int(a)
+print(type(b),b)
+c=str(a)
+print(type(c),c)
+d=float(a)
+print(type(d),d)
+e=bool(a)
+print(type(e),e)
+
+a1="Python"
+print(type(a1),a1)
+b1=str(a1)
+print(type(b1),b1)
+c1=bool(a1)
+print(type(c1),c1)
+
+a2=0.25
+print(type(a2),a2)
+b2=int(0.25)
+print(type(b2),b2)
+c2=str(a2)
+print(type(c2),c2)
+d2=bool(a2)
+print(type(d2),d2)
+
+
+a3=True
+print(type(a3),a3)
+b3=int(a3)
+print(type(b3),b3)
+c3=str(a3)
+print(type(c3),c3)
+d3=float(a3)
+print(type(d3),d3)
+
+a3=False
+print(type(a3),a3)
+b3=int(a3)
+print(type(b3),b3)
+c3=str(a3)
+print(type(c3),c3)
+d3=float(a3)
+print(type(d3),d3)
+
